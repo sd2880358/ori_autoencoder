a931c7c22499a767adfe36d895abdbb7e2efb455
lidonghao <leedonhom@gmail.com>
2021-02-23 01:47:08 -0500

    2/17

--- factorVAE/train.py
+++ factorVAE/train.py
@@ -188,6 +188,7 @@ def start_train(epochs, train_dataset, test_dataset, date, filePath):
                          + ori_cross_loss(model, test_x, d) \
                          + compute_loss(test_x) \
                          + compute_loss(r_x)
+            loss(total_loss)
         elbo = -loss.result()
         print('Epoch: {}, Test set ELBO: {}, time elapse for current epoch: {}'
               .format(epoch, elbo, end_time - start_time))
